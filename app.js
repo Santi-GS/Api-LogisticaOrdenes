6c3c0982970f2223c8d35047dc240f7dacdc73c2
Santi-GS <sgomezlr30@gmail.com>
2025-01-21 19:58:22 -0300

    Vercel

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const dbconnect = require('./config/db');
 const ordenesRoutes = require('./routes/ordenes');
 
 dotenv.config();
-const app = express();
 
+const app = express();
 app.use(express.json());
 app.use('/api', ordenesRoutes);
 
 dbconnect().then(() => {
-    app.listen(3000, () => {
-        console.log('Servidor corriendo en http://localhost:3000');
-    });
+    console.log('Base de datos conectada exitosamente');
 }).catch(err => {
-    console.error('No se pudo iniciar el servidor:', err);
+    console.error('Error al conectar la base de datos:', err);
 });
+
+module.exports = app;
